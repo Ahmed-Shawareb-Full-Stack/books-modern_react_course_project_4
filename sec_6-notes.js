e985e75921539f9b49c226c72f4b071cb099391a
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2023-01-07 02:59:06 +0200

    sections notes

---
+++ sec_6-notes.js
@@ -0,0 +1,11 @@
+//? ─── Modern React Course - Section 6 - Handle Forms ─────────────────────────────────────────
+
+/*
+project components : 
+
+ 1 - BookCreate 
+ 2 - BookList
+ 3 - BookShow
+ 4 - BookEdit
+
+*/
